3d6781372ab8e43c49c9f16d8ac51ce804c0a6d4
Averydx <avery.drennan@gmail.com>
2024-11-25 11:34:29 -0800

    modified:   pmcmc.py

--- pmcmc.py
+++ pmcmc.py
@@ -89,11 +89,6 @@ def particlemcmc_internal(data,num_particles,model_dim,init_params,init_cov,prio
 
         '''Generating the next proposal using the cholesky decompostion'''
 
-        
-        # z = rng.standard_normal((len(theta[:,iter-1])))
-        # L = cholesky((2.38**2/len(theta[:,iter - 1])) * cov)
-        #theta_prop = theta[:,iter - 1] + L @ z
-
         theta_prop = rng.multivariate_normal(theta[:,iter - 1],(2.38**2/len(theta[:,iter - 1])) * cov)
 
         LL_new = prior(theta_prop)
